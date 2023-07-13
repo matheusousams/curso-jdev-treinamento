916970b5a07510f6b2be35fec049394a051528ae
matheusousams <matheusousams.98@gmail.com>
2023-07-13 16:35:52 -0300

    commit 13/07/2023

---
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -0,0 +1,64 @@
+package br.com.springboot.curso_jdev_treinamento.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
+//import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+	
+	@Autowired //Injeção de dependência
+	private UsuarioRepository usuarioRepository;
+	
+    /**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */
+    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Curso SpringBoot API: " + name + "!";
+    }
+    
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String olaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	
+    	usuario.setNome(nome);
+    	
+    	usuarioRepository.save(usuario);
+    	
+    	return "Olá Mundo!" + nome;
+    }
+    
+    @GetMapping(value = "listaTodos")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+    	
+    }
+    
+}
